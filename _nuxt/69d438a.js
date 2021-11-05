ed9397950f43bc3450e2a3faa3e9570802564b2c
Shadhin Ahmed <shadhinplanet@gmail.com>
2021-11-05 18:20:05 +0600

    f29de13

---
+++ _nuxt/69d438a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{223:function(e,n,t){"use strict";t.r(n);var l={},o=t(51),component=Object(o.a)(l,(function(){var e=this.$createElement;return(this._self._c||e)("Home")}),[],!1,null,null,null);n.default=component.exports;installComponents(component,{Home:t(222).default})}}]);
